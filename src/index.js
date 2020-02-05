b481a6cc763277e0d4b89b058cfaa71ae0ee1c21
Noah Sylwester <nanoabram@comcast.net>
2020-02-04 16:41:58 -0800

    add authorization via brcyptjs and jsonwebtoken

--- src/index.js
+++ src/index.js
@@ -1,58 +1,26 @@
 const { GraphQLServer } = require('graphql-yoga');
+const { prisma } = require('./generated/prisma-client');
 
-let links = [{
-    id: `link-0`,
-    url: `www.howtographql.com`,
-    description: `Fullstack tutorial for GraphQL`
-}]
-
-let idCount = links.length;
+const Query = require('./resolvers/Query');
+const Mutation = require('./resolvers/Mutation');;
+const User = require('./resolvers/User');
+const Link = require('./resolvers/Link');
 
 const resolvers = {
-    Query: {
-        info: () => `here's your info`,
-        feed: () => links,
-        link: (parent, args) => {
-            let link = links.filter((item) => item.id === args.id);
-            return link;
-        }
-    },
-    Mutation: {
-        post: (parent, args) => {
-            const link = {
-                id: `link-${idCount++}`,
-                description: args.description,
-                url: args.url
-            }
-            links.push(link);
-            return link;
-        },
-        updateLink: (parent, args) => {
-            let linkIndex;
-            links.forEach((item, i) => item.id === args.id ? linkIndex = i : false);
-            links[linkIndex].url = args.url;
-            links[linkIndex].description = args.description;
-            console.log(links);
-            return links[linkIndex];
-        },
-        deleteLink: (parent, args) => {
-            let linkIndex;
-            links.forEach((item, i) => item.id === args.id ? linkIndex = i : false);
-            let deleted = linkIndex !== undefined ? links.splice(linkIndex, 1) : [null];
-            console.log(links);
-            console.log(deleted);
-            return deleted[0];
-        }
-    },
-    Link: {
-        id: (parent) => parent.id,
-        description: (parent) => parent.description,
-        url: (parent) => parent.url,
-      }
+    Query,
+    Mutation,
+    User,
+    Link,
 };
 
 const server = new GraphQLServer({
     typeDefs: `./src/schema.graphql`,
     resolvers,
+    context: request => {
+        return {
+          ...request,
+          prisma,
+        }
+      },
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
